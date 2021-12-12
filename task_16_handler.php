963f0a3ee188f7a78df989e547474d66061b0f4c
Eldaeron <eldaeron5@gmail.com>
2021-12-12 20:19:34 +0200

    Добавлено 16 задание

---
+++ task_16_handler.php
@@ -0,0 +1,15 @@
+<?php
+
+$tmp = explode('.', $_FILES['image']['name']);
+$ext = end($tmp);
+
+$image = 'uploads/' . uniqid() . ".$ext[1]";
+
+$pdo = new \PDO("mysql:host=localhost; dbname=task16", "root", "");
+$sql = "INSERT INTO images (image) VALUES (:image)";
+$statement = $pdo->prepare($sql);
+$result = $statement->execute(['image' => $image]);
+
+move_uploaded_file($_FILES['image']['tmp_name'], $image);
+
+header('location: task_16.php'); exit();
