9d314fc7511b8fce0bd2ddefcc5b765bd2ec35f6
Eldaeron <eldaeron5@gmail.com>
2021-12-12 20:35:24 +0200

    17 задание

---
+++ task_17_handler.php
@@ -0,0 +1,34 @@
+<?php
+
+if (isset($_GET['delete'])) {
+
+    $id = $_GET['delete'];
+
+    $pdo = new \PDO("mysql:host=localhost; dbname=task16", "root", "");
+    $sql = "SELECT * FROM images WHERE id = :id";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['id' => $id]);
+    $file = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if( file_exists($file['image'])) unlink($file['image']);
+
+    $sql = "DELETE FROM images WHERE id = :id";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(['id' => $id]);
+    
+    header('location: task_17.php'); exit();
+}
+
+$tmp = explode('.', $_FILES['image']['name']);
+$ext = end($tmp);
+
+$image = 'uploads/' . uniqid() . ".$ext[1]";
+
+$pdo = new \PDO("mysql:host=localhost; dbname=task16", "root", "");
+$sql = "INSERT INTO images (image) VALUES (:image)";
+$statement = $pdo->prepare($sql);
+$result = $statement->execute(['image' => $image]);
+
+move_uploaded_file($_FILES['image']['tmp_name'], $image);
+
+header('location: task_17.php'); exit();
